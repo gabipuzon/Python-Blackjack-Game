cc637f7337658bafcea8fa3b8ea51aeeeace1cd8
Gabi <gavipzn@gmail.com>
2025-07-29 15:51:51 +0800

    add card class in separate file

---
+++ game/card.py
@@ -0,0 +1,9 @@
+class Card:
+    """Represents a single playing card"""
+    
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+
+    def __str__(self):
+        return f"{self.rank['rank']} of {self.suit}"
